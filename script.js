3f6e4289993731fbdb075b2b4af4ea2fd889a0fd
Alexey <al1111997@yandex.ru>
2019-04-30 21:58:57 +0500

    'commit'

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-var question = JSON.parse(JSON.stringify(test));
-// alert(question[0].question);
-// alert(question[0].answers);
-// alert(question[0].index);
-// alert(question[0].answers[2]);
+var tasks = JSON.parse(JSON.stringify(test));
+var counter = 0;
+function showQuestions(){
+	refresh();
+	document.getElementById("begin").style.display = "none";
+	document.getElementById("next").style.display = "inline";
+	next();
+	for (var i = 0; i  < 205; i++) {
+		var questions = new Array(tasks[i].question);
+		var answers = new Array(tasks[i].answers);
+  		questions.sort();
+  		answers.sort();
+	}
+}
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function next() {
+		counter++;
+		if (counter > 29){
+			console.log("много вопросов/тест окончен");
+		}
+		else {
+			var int = getRandomInt(0, 204);
+			document.getElementById("number").innerHTML = "Вопрос "+ counter +" из 30";
+			document.getElementById("question").innerHTML = tasks[int].question;
+			document.getElementById("answers").innerHTML = tasks[int].answers;
+			var correctanswer = tasks[int].index;
+			document.getElementById("correctanswers").innerHTML = correctanswer + "(правильный ответ - потом убрать надо)";//потом убрать, нужно сравнивать
+			//нажатую кнопку ответа с этим индексом и засчитывать правильный или неправильный ответ.
+		}
+	
+}
+
+
+
+
+
+
+
+
+
+
+var sec=00;
+var min=60;
+function refresh()
+{
+	sec--;
+	if(sec==-01){
+		sec=59; min -= 1;
+	}
+	else{
+		min=min;
+	}
+	if(sec<=9){
+		sec="0" + sec;
+	}
+	time=(min<=9 ? "0"+min : min) + ":" + sec;
+	if(document.getElementById){
+		timer.innerHTML=time;
+	}
+	inter=setTimeout("refresh()", 1000);
+	if(min=='00' && sec=='00'){
+		sec="00";
+		clearInterval(inter);
+		/* выводим сообщение в элемент с id="sample", например <div id="sample"></div> */
+                //sample.innerHTML="Время вышло, тест окончен.";
+	}
+}
