f8c0347f63bcabe08d2221d2474cd59c967aefac
Alexey <al1111997@yandex.ru>
2019-04-30 10:53:37 +0500

    'commit'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var question = JSON.parse(JSON.stringify(test));
-alert(question[0].question);
-alert(question[0].answers);
-alert(question[0].index);
-alert(question[0].answers[2]);
+// alert(question[0].question);
+// alert(question[0].answers);
+// alert(question[0].index);
+// alert(question[0].answers[2]);
