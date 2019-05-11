adbbe4d476f0527f8c17874665978f4b2670149a
Roman Chumakov <chumakoff.r.v@gmail.com>
2019-05-11 15:49:53 +0500

    Добавляет обработку логов и вывод результата на экран.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var tasks = JSON.parse(JSON.stringify(test));
 var counter = 0;
+var result = [];
+var i;
+var correctanswer;
+var int;
 function showQuestions(){
 	if(document.getElementById("name").value === '') {
     	alert("Напиши свое имя!");
 	}
 	else {
+		document.getElementById("test").className = "test";
+		document.getElementById("hello").className = "hidden";
 		refresh();
 		document.getElementById("begin").style.display = "none";
 		document.getElementById("next").style.display = "inline-block";
@@ -24,24 +30,99 @@ function getRandomInt(min, max) {
 }
 
 function next() {
-		counter++;
-		if (counter > 30){
-			console.log("тест окончен");
-		}
-		else {
-			document.getElementById("answers").innerHTML = "";
-			var answer;
-			var int = getRandomInt(0, 204);
-			document.getElementById("number").innerHTML = "<br>" + "Вопрос "+ counter +" из 30";
-			document.getElementById("question").innerHTML = tasks[int].question;
-			for (answer of tasks[int].answers) {
-				document.getElementById("answers").innerHTML += ' <input type="radio" value=""></input> ' + answer + '<br>';
+	document.getElementById("answers").style = "animation-name: no";
+	if (counter > 0) {//проверка ответа
+		for (i = 0; i < tasks[int].answers.length; i++) {
+			let element = document.getElementById(`answer-${counter}-${i + 1}`);
+			if (element.checked) {
+				if (+element.value === +correctanswer){
+					if (counter %2 === 0) {
+						document.getElementById("answers").style = "animation-name: green;";
+					}
+					else {
+						document.getElementById("answers").style = "animation-name: green2;";
+					}
+				}
+				else {
+					if (counter %2 === 0) {
+						document.getElementById("answers").style = "animation-name: red;";
+					}
+					else {
+						document.getElementById("answers").style = "animation-name: red2;";
+					}
+				}
+				result.push({
+					number: int,
+					order: counter,
+					userAnswer: element.value,
+					rightAnswer: correctanswer
+				});
+				break;
 			}
-			var correctanswer = tasks[int].index;
-			document.getElementById("correctanswers").innerHTML = correctanswer + "(правильный ответ - потом убрать надо)";//потом убрать, нужно сравнивать
-			//нажатую кнопку ответа с этим индексом и засчитывать правильный или неправильный ответ.
 		}
-	
+	}
+	counter++;
+	if (counter > 30){
+		complete();
+	}
+	else {
+		document.getElementById("answers").innerHTML = "";
+		int = getRandomInt(0, 204);
+		document.getElementById("number").innerHTML = "<br>" + "Вопрос "+ counter +" из 30";
+		document.getElementById("question").innerHTML = tasks[int].question;
+		for (i = 0; i < tasks[int].answers.length; i++) {
+			document.getElementById("answers").innerHTML += ` <input type="radio" id="answer-${counter}-${i+1}" value="${i+1}"></input>${tasks[int].answers[i]}<br> `;
+		}
+		correctanswer = tasks[int].index;
+	}
+
+}
+
+function complete() {
+
+	let res = 0;
+
+	document.getElementById("result").className = "";
+	document.getElementById("test").className = "hidden";
+	let table = document.getElementById("result-table");
+	let tr = document.createElement("tr");
+	table.appendChild(tr);
+	let td = document.createElement("td");
+	td.appendChild(document.createTextNode("Номер вопроса"));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode("Правильность ответа"));
+	tr.appendChild(td);
+
+	for (i = 0; i < result.length; i++) {
+		let tr = document.createElement("tr");
+		tr.className = +result[i].userAnswer === +result[i].rightAnswer ? "green" : "red";
+		table.appendChild(tr);
+		let td = document.createElement("td");
+		td.appendChild(document.createTextNode(result[i].order));
+		tr.appendChild(td);
+		td = document.createElement("td");
+		td.appendChild(document.createTextNode(+result[i].userAnswer === +result[i].rightAnswer ? "Да" : "Нет"));
+		console.log(result[i].userAnswer, result[i].rightAnswer);
+		tr.appendChild(td);
+		res += +result[i].userAnswer === +result[i].rightAnswer ? 1 : 0;
+	}
+
+	let rowLeft = 30 - result.length;
+	for (i = 0; i < rowLeft; i++) {
+		let tr = document.createElement("tr");
+		tr.className = "red";
+		table.appendChild(tr);
+		let td = document.createElement("td");
+		td.appendChild(document.createTextNode(result.length + i + 1));
+		tr.appendChild(td);
+		td = document.createElement("td");
+		td.appendChild(document.createTextNode("Нет ответа"));
+		tr.appendChild(td);
+	}
+
+	document.getElementById("result-brief").appendChild(document.createTextNode(`Ваш результат: ${Math.round(res/30*100)}%`));
+	//сделать выгрузку в файл
 }
 
 var sec=00;
@@ -68,5 +149,6 @@ function refresh()
 		clearInterval(inter);
 		/* выводим сообщение в элемент с id="sample", например <div id="sample"></div> */
                 //sample.innerHTML="Время вышло, тест окончен.";
+		complete();
 	}
 }
